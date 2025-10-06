75951312433dc5c5c8943966938a3b2047609d0e
H0NEP0T-466 <fa-23-bscs-466@lgu.edu.pk>
2025-10-06 19:58:26 +0500

    modelCHANGING

--- backend/gemini/notesgemini.js
+++ backend/gemini/notesgemini.js
@@ -6,9 +6,9 @@ const ai = new GoogleGenAI({
 });
 
 const CANDIDATE_MODELS = [
+  "gemini-2.5-pro",
   "gemini-2.5-flash",
   "gemini-2.0-flash", 
-  "gemini-2.5-pro",
   "gemini-1.5-flash",
   "gemini-1.5-pro"
 ];
